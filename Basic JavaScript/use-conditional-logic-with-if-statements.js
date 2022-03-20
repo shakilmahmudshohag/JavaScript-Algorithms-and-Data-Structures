d00be4692eb042f64a6fd84df23f07a48a82014c
shakilmahmudshohag <70774510+shakilmahmudshohag@users.noreply.github.com>
2022-03-20 15:00:16 +0600

    Add files via upload

---
+++ Basic JavaScript/use-conditional-logic-with-if-statements.js
@@ -0,0 +1,12 @@
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue){
+  return "Yes, that was true";
+}
+return "No, that was false";
+}
+trueOrFalse(true);
+trueOrFalse(false);
+
+  // Only change code above this line
+
