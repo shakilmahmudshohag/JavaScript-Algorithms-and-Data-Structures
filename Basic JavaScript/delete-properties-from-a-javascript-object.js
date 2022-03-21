46c687bf38afcf37259c720487cad60f4534d4e1
MD Shakil Mahmud Shohag <70774510+shakilmahmudshohag@users.noreply.github.com>
2022-03-21 21:30:03 +0600

    Add files via upload

---
+++ Basic JavaScript/delete-properties-from-a-javascript-object.js
@@ -0,0 +1,10 @@
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
+delete myDog.tails;
+// Only change code below this line
---
+++ Basic JavaScript/updating-object-properties.js
@@ -0,0 +1,9 @@
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+myDog.name="Happy Coder";
+// Only change code below this line
